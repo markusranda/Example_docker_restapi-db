40fe4fdf7d4efe1e28c689952b86cd456c4ef22c
Markus Randa <randamarkus.95@gmail.com>
2020-01-14 15:08:18 +0100

    Users can create new accounts using endpoint! ! !

--- Mapping/ResourceToModelProfile.cs
+++ Mapping/ResourceToModelProfile.cs
@@ -1,6 +1,8 @@
 using AutoMapper;
 using Supermarket.API.Domain.Models;
+using Supermarket.API.Domain.Models.Auth;
 using Supermarket.API.Resources;
+using Supermarket.API.Resources.Auth;
 
 namespace Supermarket.API.Mapping {
 
@@ -8,6 +10,8 @@ namespace Supermarket.API.Mapping {
         public ResourceToModelProfile()
         {
             CreateMap<SaveCategoryResource, Category>();
+            
+            CreateMap<UserCredentialResource, User>();
         }
     }
 
